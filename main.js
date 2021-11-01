62dd8168b11594ca1f8c7b4a39103c2bb9f9b996
Revenge_Master <69684754+tosbas@users.noreply.github.com>
2021-11-01 18:23:32 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,205 +1,154 @@
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
+const messageBox = document.getElementById("messageBox");
+const bouton_start = document.getElementById("bouton_start");
 
-canvas.width = 350;
-canvas.height = 580;
+canvas.width = 300;
+canvas.height = 450;
 
 if (window.innerWidth > 600) {
-    canvas.width = 600;
-    canvas.height = 600;
+    canvas.width = 580;
+} else {
+    canvas.width = 300;
 }
-
-const messageBox = document.querySelector("#messageBox");
-const bouton_start = document.querySelector("#bouton_start");
-
-let score = document.querySelector("#score");
-let best_score = document.querySelector("#best_score");
-
-
-
-let scoreParse = parseInt("0");
-best_score.innerText = scoreParse;
-
-localStorage.setItem("BestScore", "0");
-best_score.innerText = localStorage.getItem("BestScore", scoreParse);
-
-score.innerText = 0;
 
 const image = new Image();
 image.src = "flappy.png";
 
-let start = false;
+/**************************************************************************** Général */
+
+let start;
+let score = 0;
+let best_score = 0;
+
+/******************************************************************************* flappy */
+
+class bird {
+    constructor(x, y, width, height, pushDown, pushUp) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.pushDown = 1.3;
+        this.pushUp = 50;
+    }
+    draw() {
+        ctx.drawImage(image, 430, 265, 150, 100, this.x, this.y, this.width, this.height);
+    }
+    method() {
+        this.y += this.pushDown;
+
+        if (this.y > canvas.height - 20 - this.height ||
+            this.x + this.width >= pipe.x && this.y + this.height >= pipe.y && this.x <= pipe.x + pipe.width ||
+            this.x + this.width > pipe2.x && this.y > pipe2.y && this.y < pipe2.y + pipe2.height && this.y < pipe2.width && this.x < pipe2.x + pipe2.width) {
+            start = false;
+            score = 0;
+        }
+
+    }
+}
+
+const flappyBird = new bird(canvas.width / 2 - 12.5, canvas.height / 2 - 12.5, 25, 25);
+
+/********************************************************************************* pipes */
+class pipes {
+    constructor(x, y, width, height, pushLeft) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.pushLeft = 0.7;
+    }
+    draw() {
+        ctx.drawImage(image, 120, 50, 165, 860, this.x, this.y, this.width, this.height);
+        //sol
+        ctx.drawImage(image, 0, 950, canvas.width, 50, 0, canvas.height - 20, canvas.width, 20);
+    }
+    method() {
+        this.x -= this.pushLeft;
+    }
+}
+
+// function pour sortie un nombre aléatoire compris entre un minimum et un maximum
+function minMaxNumber(min, max) {
+    let value = Math.floor(Math.random() * (max - min)) + min;
+    return value;
+}
+
+const pipe = new pipes(canvas.width + 300, minMaxNumber(150, 350), 100, 300);
+const pipe2 = new pipes(canvas.width + 300, minMaxNumber(-280, -150), 100, 300);
+
+/****************************************************************************** function */
+
+// dessine le décors
+function drawStatiqueImage() {
+    requestAnimationFrame(drawStatiqueImage);
+    //background
+    ctx.drawImage(image, 800, 0, 500, 1000, 0, 0, canvas.width, canvas.height);
+
+
+    document.getElementById("score").innerText = score;
+    localStorage.setItem("best_score", best_score);
+    document.getElementById("best_score").innerText = best_score;
+
+    // flappy
+    flappyBird.draw();
+
+    pipe.draw();
+    pipe2.draw();
+
+    if (pipe.x < 0 - pipe.width && pipe2.x < 0 - pipe2.width) {
+        score++;
+        if (score > best_score) {
+            best_score++;
+        }
+
+        pipe.x = canvas.width;
+        pipe.y = minMaxNumber(150, 350);
+        pipe2.y = minMaxNumber(-280, -150);
+        pipe2.x = canvas.width;
+
+    }
+}
+
+function colision() {
+
+    if (start != false) {
+        requestAnimationFrame(colision);
+        flappyBird.method();
+        pipe.method();
+        pipe2.method();
+    } else {
+        messageBox.classList.remove("cacheText");
+        messageBox.textContent = "Game Over !";
+        messageBox.appendChild(bouton_start);
+        score = 0;
+    }
+}
+
+/****************************************************************************** Game */
+
+drawStatiqueImage();
 
 bouton_start.addEventListener("click", () => {
-    messageBox.classList.add("cacheText");
+    flappyBird.y = canvas.height / 2;
+    pipe.x = canvas.width + 300;
+    pipe2.x = canvas.width + 300;
     start = true;
-    pillier_x = canvas.width;
-
+    colision();
+    messageBox.classList.toggle("cacheText");
 })
 
-// **********************************************Globale**************************************************
-
-// dessine dés de le départ le background et le sol
-
-function drawImage() {
-    requestAnimationFrame(drawImage)
-
-    // background
-    ctx.drawImage(image, 750, 300, canvas.width, canvas.height, 0, 0, canvas.width, canvas.height);
-
-    // sol
-    ctx.drawImage(image, 0, 950, canvas.width, 50, 0, 550, canvas.width, 50);
-
-}
-
-drawImage()
-
-// **************************************************Pillier**************************************************
-
-function returnRandomNumber(min, max) {
-    return Math.random() * (max - min) + min;
-}
-
-let pillier_y = returnRandomNumber(150, 500);
-let pillier_x = canvas.width;
-
-let pillier_bas = true;
-let pillier_haut = false;
-
-let speedInc = 1;
-
-function drawPillier() {
-    requestAnimationFrame(drawPillier)
-    if (start == true) {
-
-        pillier_x -= speedInc;
-
-        ctx.drawImage(image, 110, 30, 200, 600, pillier_x, pillier_y, 200, 600);
-
-        // je doit remettre le sol sinon le pillier passe devant
-        ctx.drawImage(image, 0, 950, canvas.width, 50, 0, 550, canvas.width, 50);
-
-
-        if (pillier_x < -200) {
-            score.innerText++;
-            if (score.innerText > scoreParse) {
-                scoreParse += 1;
-                localStorage.setItem("BestScore", scoreParse);
-
-                best_score.innerText = localStorage.getItem("BestScore", scoreParse);
-            }
-
-            pillier_x = canvas.width;
-
-            if (pillier_y > 0) {
-                pillier_y = returnRandomNumber(-100, -400);
-                pillier_haut = true;
-                pillier_bas = false;
-
-            } else if (pillier_y < 0) {
-                pillier_y = returnRandomNumber(100, 500);
-                pillier_bas = true;
-                pillier_haut = false;
-
-            }
-        }
-    }
-}
-
-drawPillier();
-
-// **************************************************Flappy**************************************************
-
-// frame de flappy
-
-let frame = 265;
-
-// function qui me permet de changer la frame tous les 150ms
-
-function frameFlappy() {
-
-    setInterval(() => {
-        if (start == true) {
-
-            if (frame == 265) {
-                frame = 590
-            } else if (frame == 590) {
-                frame = 265;
-            }
-        }
-
-    }, 150)
-
-}
-
-let flappy_x = 150;
-let flappy_y = 100;
-
-// function pour gérer les collisions
-function collisionFlappy() {
-    if (flappy_y > canvas.height || flappy_y < -30 || flappy_x + 50 > pillier_x && flappy_y + 30 > pillier_y && flappy_x + 40 < pillier_x + 230 && pillier_bas == true || flappy_x + 50 > pillier_x && flappy_y > pillier_y && flappy_y < pillier_y + 600 && flappy_x + 50 < pillier_x + 230 && pillier_haut == true) {
-
-        start = false;
-        GameOver();
-
-
-    }
-
-}
-
-
-//function en attente, dés que start est égale à true alors flappy tombe est la frame de flappy change
-function moveFlappy() {
-    requestAnimationFrame(moveFlappy)
-    if (start == true) {
-        flappy_y += 1.2;
-        setInterval(flappy_y += 0.2)
-        frameFlappy();
-
-    }
-}
-
-moveFlappy();
-
-// function qui s'appelle elle-même, affichage de flappy
-
-function drawFlappy() {
-    requestAnimationFrame(drawFlappy)
-        // sx,sy,sw,sh,dx,dy,dw,dh
-    ctx.drawImage(image, 434, frame, 150, 150, flappy_x, flappy_y, 52, 60);
-
-    if (start == true) {
-        collisionFlappy();
-    }
-
-}
-
-
-drawFlappy()
-
-
-// event sur l'appuie souris pour remonter flappy
 canvas.addEventListener("click", () => {
     if (start == true) {
-        flappy_y -= 50;
+        if (flappyBird.y - flappyBird.height < 0) {
+            flappyBird.y -= 0;
+        } else {
+            let audio = document.createElement("audio");
+            document.querySelector("figure").appendChild(audio);
+            audio.innerHTML = "<audio controls autoplay src = 'sound.mp3'>"
+            flappyBird.y -= 45;
+        }
     }
 
 })
-
-// *********************************************Game Over*******************************
-
-function GameOver() {
-    score.innerText = 0;
-    flappy_x = 150;
-    flappy_y = 100;
-    speedInc = 1;
-    pillier_y = returnRandomNumber(100, 500);
-    messageBox.classList.remove("cacheText");
-    messageBox.innerHTML = "Game Over<br>Rejouer ?";
-    messageBox.appendChild(bouton_start);
-}
-
-setInterval(() => {
-    speedInc += 0.1
-
-}, 1000)
