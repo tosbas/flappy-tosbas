c12b90e9f8184a21a3b8bf3785015efe5d047d73
Desforges Thomas <69684754+tosbas@users.noreply.github.com>
2023-07-16 16:07:37 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -3,71 +3,118 @@ const ctx = canvas.getContext("2d");
 const messageBox = document.getElementById("messageBox");
 const bouton_start = document.getElementById("bouton_start");
 
-canvas.height = 450;
+canvas.height = 600;
 
-if (window.innerWidth > 600) {
-    canvas.width = 580;
-} else {
-    canvas.width = 300;
+
+if(window.innerWidth > 800){
+    canvas.width = 800;
+}else{
+    canvas.width = window.innerWidth;
 }
+
+window.addEventListener("resize", () => {
+
+    if(window.innerWidth > 800){
+        canvas.width = 800;
+    }else{
+        canvas.width = window.innerWidth;
+    }
+})
+
+
+
 
 const image = new Image();
 image.src = "flappy.png";
 
-/**************************************************************************** Général */
+//************************************ Général *****************************************//
 
-let start;
+let start = false;
 let score = 0;
 let best_score = 0;
-let pushLeft = 1.2;
 let sound = false;
 
-/******************************************************************************* flappy */
+//**************************************  flappy ******************************************//
 
 class bird {
-    constructor(x, y, width, height, pushDown, pushUp, frame) {
+    constructor(x, y, width, height) {
         this.x = x;
         this.y = y;
+        this.vx = 0;
+        this.vy = 0;
         this.width = width;
         this.height = height;
-        this.pushDown = 1.3;
-        this.pushUp = 45;
+        this.pushDown = 0.1;
+        this.pushLeft = 0.5;
+        this.pushUp = 5;
         this.frame = 265;
     }
     draw() {
         ctx.drawImage(image, 430, this.frame, 150, 100, this.x, this.y, this.width, this.height);
     }
     method() {
-        this.y += this.pushDown;
+
+        this.vy += this.pushDown;
 
         if (this.y > canvas.height - 20 - this.height ||
-            this.x + this.width >= pipe.x && this.y + this.height >= pipe.y && this.x <= pipe.x + pipe.width ||
-            this.x + this.width >= pipe2.x && this.y > pipe2.y && this.y < pipe2.y + pipe2.height && this.x <= pipe2.x + pipe2.width) {
+            this.x + this.width >= pipe.pipe1X && this.y + this.height >= pipe.pipe1Y && this.x <= pipe.pipe1X + pipe.pipe1W ||
+            this.y <= -pipe.pipe2Y && this.x + this.width >= pipe.pipe2X && this.x < pipe.pipe2X + pipe.pipe2W) {
             start = false;
             score = 0;
+
+            messageBox.classList.remove("cacheText");
+            messageBox.textContent = "Game Over !";
+            messageBox.appendChild(bouton_start);
+            score = 0;
+
         }
+
+        if (this.y <= 0) {
+            this.vy += this.pushUp;
+        }
+
+        if (start) {
+            this.y += this.vy;
+        }
+
 
     }
 }
 
 const flappyBird = new bird(canvas.width / 2 - 12.5, canvas.height / 2 - 12.5, 25, 25);
 
-/********************************************************************************* pipes */
-class pipes {
+//*********************************** areaGame ***********************************************//
+class areaGame {
     constructor(x, y, width, height) {
         this.x = x;
         this.y = y;
         this.width = width;
         this.height = height;
+        this.pushLeft = 5;
+        this.pipe1X = this.x;
+        this.pipe1Y = this.y;
+        this.pipe1W = this.width;
+        this.pipe1H = this.height;
+        this.ecart = 100;
+        this.pipe2X = this.x;
+        this.pipe2Y = -this.pipe1Y + this.ecart;
+        this.pipe2W = this.width;
+        this.pipe2H = this.height;
     }
     draw() {
-        //pipe
-        ctx.drawImage(image, 114, 40, 180, 880, this.x, this.y, this.width, this.height);
+
+        ctx.drawImage(image, 100, 30, 200, 880, this.pipe1X, this.pipe1Y, this.pipe1W, this.pipe1H);
+        ctx.save();
+        ctx.scale(1, -1);
+        ctx.drawImage(image, 100, 30, 200, 800, this.pipe2X, this.pipe2Y, this.pipe2W, this.pipe2H);
+        ctx.restore();
+
         //sol
-        ctx.drawImage(image, 0, 950, canvas.width, 50, 0, canvas.height - 20, canvas.width, 20);
+        ctx.drawImage(image, 0, 950, canvas.width, 50, 0, canvas.height - 20, 960, 20);
     }
     method() {
-        this.x -= pushLeft;
+        this.pipe1X -= this.pushLeft;
+        this.pipe2X -= this.pushLeft;
     }
 }
 
@@ -77,14 +124,19 @@ function minMaxNumber(min, max) {
     return value;
 }
 
-const pipe = new pipes(canvas.width + 300, minMaxNumber(180, 360), 50, 300);
-const pipe2 = new pipes(canvas.width + 300, pipe.y - 400, 50, 300);
+const pipe = new areaGame(canvas.width + 300, minMaxNumber(180, 500), 80, 400);
 
-/****************************************************************************** function */
+//*********************************** function  ********************************************//
 
-// dessine le décors
-function drawStatiqueImage() {
-    requestAnimationFrame(drawStatiqueImage);
+
+const gameLoop = () => {
+
+
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+
+    requestAnimationFrame(gameLoop);
+
     //background
     ctx.drawImage(image, 800, 0, 500, 1000, 0, 0, canvas.width, canvas.height);
 
@@ -92,92 +144,76 @@ function drawStatiqueImage() {
     localStorage.setItem("best_score", best_score);
     document.getElementById("best_score").innerText = best_score;
 
-    // flappy
     flappyBird.draw();
-
     pipe.draw();
-    pipe2.draw();
 
-    if (pipe.x < 0 - pipe.width && pipe2.x < 0 - pipe2.width) {
+    if (pipe.pipe1X < 0 - pipe.pipe1W && pipe.pipe2X < 0 - pipe.pipe2W) {
         score++;
+
         if (score > best_score) {
             best_score++;
         }
 
+        pipe.pipe1X = canvas.width + 300;
+        pipe.pipe2X = canvas.width + 300;
 
-        pipe.x = canvas.width + 100;
-        pipe2.x = canvas.width + 100;
-
-        pipe.y = minMaxNumber(180, 360);
-        pipe2.y = pipe.y - 400;
-
+        pipe.pipe1Y = minMaxNumber(180, 500);
+        pipe.pipe2Y = -pipe.pipe1Y + pipe.ecart;
     }
-}
 
-function colision() {
-
-    if (start != false) {
-        requestAnimationFrame(colision);
+    if (start) {
         flappyBird.method();
         pipe.method();
-        pipe2.method();
-
-
-    } else {
-        messageBox.classList.remove("cacheText");
-        messageBox.textContent = "Game Over !";
-        messageBox.appendChild(bouton_start);
-        score = 0;
     }
 }
 
-/****************************************************************************** Game */
+gameLoop();
 
-drawStatiqueImage();
+//*********************************** Game ********************************************//
 
 bouton_start.addEventListener("click", () => {
+    pipe.pushLeft = 5;
     flappyBird.y = canvas.height / 2;
-    pipe.x = canvas.width + 300;
-    pipe2.x = canvas.width + 300;
+    pipe.pipe1X = canvas.width + 300;
+    pipe.pipe2X = canvas.width + 300;
     start = true;
-    pushLeft = 2;
-    setInterval(() => {
-        pushLeft += 0.1;
-    }, 20000)
-    colision();
     messageBox.classList.toggle("cacheText");
-
 })
 
 canvas.addEventListener("click", () => {
-    if (start == true) {
-        if (flappyBird.y - flappyBird.height < 0) {
-            flappyBird.y -= 0;
-        } else {
-            let audio = document.createElement("audio");
-            document.querySelector("figure").appendChild(audio);
-            if (sound == true) {
-                audio.innerHTML = "<audio controls autoplay src = 'sound.mp3'>";
-            }
 
-            flappyBird.y -= flappyBird.pushUp;
-            setInterval(() => {
-                if (flappyBird.frame == 265) {
-                    flappyBird.frame = 425;
-                } else if (flappyBird.frame == 425) {
-                    flappyBird.frame = 265;
-                }
+    if (start) {
 
-            }, 500);
-
-
+        let audio = document.createElement("audio");
+        document.querySelector("figure").appendChild(audio);
+        if (sound == true) {
+            audio.innerHTML = "<audio controls autoplay src = 'sound.mp3'>";
         }
 
+        flappyBird.vy -= flappyBird.pushUp;
     }
 
 })
 
-/****************************************************** bouton sound */
+
+setInterval(() => {
+    if (start) {
+        if (flappyBird.frame == 265) {
+            flappyBird.frame = 425;
+        } else if (flappyBird.frame == 425) {
+            flappyBird.frame = 265;
+        }
+    }
+}, 200);
+
+setInterval(() => {
+    if (start) {
+        pipe.pushLeft += 0.1;
+    }
+
+}, 10000)
+
+/*********************** bouton sound ********************************/
 
 const container = document.getElementById("container");
 const circle = document.getElementById("circle");
